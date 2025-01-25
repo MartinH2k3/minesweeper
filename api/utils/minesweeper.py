21a67b3480b625da6e95622b7087c04a57bb9db1
Martin Horský <87446996+MartinH2k3@users.noreply.github.com>
2025-01-25 14:49:22 +0100

    backtracking with splitting into islands, aided by GPT

--- api/utils/minesweeper.py
+++ api/utils/minesweeper.py
@@ -89,16 +89,196 @@ class Board:
         return revealed
 
     def auto_solve_simple(self):
-        while self.auto_flag_simple() + self.auto_reveal_simple():
-            pass
+        flagged = self.auto_flag_simple()
+        revealed = self.auto_reveal_simple()
+        while flagged + revealed:
+            flagged = self.auto_flag_simple()
+            revealed = self.auto_reveal_simple()
+        return flagged + revealed
 
     def auto_solve_advanced(self):
-        pass
+        """
+            Identify islands bordering unrevealed fields and do partial backtracking
+            to find forced flags or reveals.
+            Returns number of cells changed (flagged or revealed).
+        """
+        frontier = self._get_frontier()
+        islands = self._get_islands(frontier)
+        total_changed = 0
+        for island in islands:
+            constraints = self._get_constraints_for_island(island)
+            changed = self._process_island_with_backtracking(island, constraints)
+            total_changed += changed
+
+        return total_changed
+
+    def _get_frontier(self):
+        frontier = []
+        for i in range(self.size):
+            for j in range(self.size):
+                if not self.revealed[i][j] and Point(i, j) not in self.flags:
+                    # Check if it's adjacent to any revealed cell
+                    for p in Point(i, j).neighbors(True):
+                        if p.within(self.board) and self.revealed[p.x][p.y]:
+                            frontier.append(Point(i, j))
+                            break
+        return frontier
+
+    def _get_islands(self, frontier):
+        frontier_set = set(frontier)  # for quick 'in' checks
+        visited = set()
+        islands = []
+
+        def dfs(start):
+            stack = [start]
+            island = set()
+            while stack:
+                cell = stack.pop()
+                if cell in visited:
+                    continue
+                visited.add(cell)
+                island.add(cell)
+                # check neighbors that are in frontier
+                for nb in cell.neighbors(True):
+                    if nb in frontier_set and nb not in visited:
+                        stack.append(nb)
+            return island
+
+        for cell in frontier:
+            if cell not in visited:
+                island = dfs(cell)
+                islands.append(island)
+
+        return islands
+
+    def _get_constraints_for_island(self, island):
+        """
+        Return a list of constraints.
+        Each constraint is (hidden_cells_list, required_mines).
+        """
+        island_constraints = []
+        used_constraints = set()
+
+        for cell in island:
+            for nb in cell.neighbors(True):
+                if nb.within(self.board) and self.revealed[nb.x][nb.y]:
+                    if nb not in used_constraints:
+                        used_constraints.add(nb)
+                        # Now build the constraint
+                        hidden_list = []
+                        flagged_count = 0
+                        required_mines = self.board[nb.x][nb.y]  # the number on that revealed cell
+                        for n2 in nb.neighbors(True):
+                            if n2.within(self.board):
+                                if n2 in self.flags:
+                                    flagged_count += 1
+                                elif not self.revealed[n2.x][n2.y]:
+                                    hidden_list.append(n2)
+                        required_mines -= flagged_count
+                        island_constraints.append((hidden_list, required_mines))
+
+        return island_constraints
+
+    def _process_island_with_backtracking(self, island, constraints):
+        """
+        1) Build a list of island cells (variables).
+        2) Enumerate all solutions that satisfy 'constraints'.
+        3) For each cell, check if it's always a mine or always safe.
+        4) Flag or reveal accordingly.
+        Return how many cells changed.
+        """
+        island_cells = list(island)
+        n = len(island_cells)
+        valid_solutions = []
+
+        # We'll store partial assignments in a list of 0/1, same order as island_cells
+        assignment = [0] * n
+
+        def backtrack(index):
+            if index == n:
+                # Check constraints
+                if self._check_constraints(assignment, island_cells, constraints):
+                    valid_solutions.append(assignment[:])  # copy
+                return
+
+            # Option 1: cell is not a mine
+            assignment[index] = 0
+            # quick prune check - optional advanced logic
+            if self._partial_ok(assignment, index, island_cells, constraints):
+                backtrack(index + 1)
+
+            # Option 2: cell is a mine
+            assignment[index] = 1
+            # prune check again
+            if self._partial_ok(assignment, index, island_cells, constraints):
+                backtrack(index + 1)
+
+        backtrack(0)
+
+        if not valid_solutions:
+            # No valid solutions found => no forced moves
+            return 0
+
+        # For each cell, check if it's always 1 or always 0
+        changed = 0
+        for i, cell in enumerate(island_cells):
+            all_1 = all(sol[i] == 1 for sol in valid_solutions)
+            all_0 = all(sol[i] == 0 for sol in valid_solutions)
+            if all_1 and cell not in self.flags:
+                self.flag(cell)
+                changed += 1
+            elif all_0 and not self.revealed[cell.x][cell.y] and cell not in self.flags:
+                self.reveal(cell)
+                changed += 1
+        return changed
+
+    def _check_constraints(self, assignment, island_cells, constraints):
+        assign_map = {}
+        for i, c in enumerate(island_cells):
+            assign_map[c] = assignment[i]
+        for f in self.flags:
+            assign_map[f] = 1
+
+        for (hidden_list, needed) in constraints:
+            count = 0
+            for h in hidden_list:
+                if h in assign_map:
+                    count += assign_map[h]  # either 0 or 1
+                # if h not in assign_map => assume 0 or interpret differently
+            if count != needed:
+                return False
+        return True
+
+    def _partial_ok(self, assignment, index, island_cells, constraints):
+        # For a quick check, see if any constraint is exceeded so far
+        assign_map = {}
+        for i, c in enumerate(island_cells):
+            if i <= index:
+                assign_map[c] = assignment[i]
+        for f in self.flags:
+            assign_map[f] = 1
+
+        for (hidden_list, needed) in constraints:
+            assigned_sum = 0
+            unknown = 0
+            for h in hidden_list:
+                if h in assign_map:
+                    assigned_sum += assign_map[h]
+                else:
+                    unknown += 1
+            if assigned_sum > needed:
+                return False
+            if assigned_sum + unknown < needed:
+                return False
+        return True
 
     def auto_solve(self):
         while sum(sum(i) for i in self.revealed) + self.mine_count < self.size**2:
-            self.auto_solve_simple()
-            self.auto_solve_advanced()
+            changed_simple = self.auto_solve_simple()
+            changed_advanced = self.auto_solve_advanced()
+            if changed_simple + changed_advanced == 0:
+                # No more progress
+                break
 
 
     def __str__(self):
@@ -153,7 +333,7 @@ class MinesweeperGUI:
         self.hint_button = tk.Button(
             self.root, text="Hint",
             font=("Helvetica", 12),
-            command=lambda: (self.board.auto_solve_simple(), self.update_ui())
+            command=lambda: (self.board.auto_solve(), self.update_ui())
         )
         self.hint_button.grid(row=self.size + 1, column=1, columnspan=self.size)
         self.play_again_button = None
